c1226e8b1de3a8865fcac02ee99746cf9d284b99
valeria-knv <115754671+valeria-knv@users.noreply.github.com>
2023-06-22 19:01:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import telebot
 import sqlite3
 
 bot = telebot.TeleBot('6197503153:AAHX9Yz5w1bpDs7v3KlplIye1hg9JVrFQlc')
-# role = None
+role = None
 name = None
 lastname = None
 password = None
 company = None
 team = None
 nickname = None
+check = False
 
 
 class Database:
@@ -17,20 +18,23 @@ class Database:
         self.connection = sqlite3.connect(db_name, check_same_thread=False)
         self.cursor = self.connection.cursor()
 
-    def create_table_user(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT NOT NULL, "
-                            "lastname TEXT NOT NULL, password TEXT NOT NULL)")
+    def create_table(self):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY)")
         self.connection.commit()
 
     def add_user(self, message):
         try:
-            self.cursor.execute("INSERT INTO users (id, name, lastname, password) VALUES (?, ?, ?, ?)",
-                                (message.chat.id, name, lastname, password))
+            self.cursor.execute("INSERT INTO users (id) VALUES (?)", (message.chat.id,))
             self.connection.commit()
         except sqlite3.IntegrityError:
             bot.send_message(message.chat.id, 'Ви вже зареєстровані!')
 
-        bot.send_message(message.chat.id, 'Ви зареєстровані!')
+    def user_exists(self, message):
+        try:
+            result = self.cursor.execute("SELECT * FROM users WHERE id = ?", (message.chat.id,)).fetchall()
+            return bool(result)
+        except sqlite3.IntegrityError:
+            bot.send_message(message.chat.id, 'Ви вже зареєстровані!')
 
     def create_table_organizer(self):
         self.cursor.execute("CREATE TABLE IF NOT EXISTS organizers (id INTEGER PRIMARY KEY, name TEXT NOT NULL, "
@@ -42,17 +46,16 @@ class Database:
             self.cursor.execute("INSERT INTO organizers (id, name, lastname, password) VALUES (?, ?, ?, ?)",
                                 (message.chat.id, name, lastname, password))
             self.connection.commit()
+            bot.send_message(message.chat.id, 'Ви зареєстровані!')
         except sqlite3.IntegrityError:
             bot.send_message(message.chat.id, 'Ви вже зареєстровані!')
-
-        bot.send_message(message.chat.id, 'Ви зареєстровані!')
 
     def create_table_jury(self):
         self.cursor.execute("CREATE TABLE IF NOT EXISTS jury (id INTEGER PRIMARY KEY, name TEXT NOT NULL, "
                             "lastname TEXT NOT NULL, company TEXT, contest_id INTEGER, password TEXT NOT NULL)")
         self.connection.commit()
 
-    def add_user_jury(self, message, check):
+    def add_user_jury(self, message):
         try:
             if check:
                 self.cursor.execute("INSERT INTO jury (id, name, lastname, company, password) VALUES (?, ?, ?, ?, ?)",
@@ -61,25 +64,36 @@ class Database:
                 self.cursor.execute("INSERT INTO jury (id, name, lastname, password) VALUES (?, ?, ?, ?)",
                                     (message.chat.id, name, lastname, password))
             self.connection.commit()
+            bot.send_message(message.chat.id, 'Ви зареєстровані!')
         except sqlite3.IntegrityError:
             bot.send_message(message.chat.id, 'Ви вже зареєстровані!')
 
-        bot.send_message(message.chat.id, 'Ви зареєстровані!')
-
     def create_table_participant(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS participants (id INTEGER PRIMARY KEY, name TEXT NOT NULL, "
-                            "lastname TEXT NOT NULL, team TEXT NOT NULL, contest_id INTEGER, password TEXT NOT NULL)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS participants (id INTEGER PRIMARY KEY, team TEXT NOT NULL, contest_id INTEGER, password TEXT NOT NULL)")
         self.connection.commit()
 
     def add_user_participant(self, message):
         try:
-            self.cursor.execute("INSERT INTO participants (id, name, lastname, team, password) VALUES (?, ?, ?, ?, ?)",
-                                (message.chat.id, name, lastname, team, password))
+            self.cursor.execute("INSERT INTO participants (id, team, password) VALUES (?, ?, ?)",
+                                (message.chat.id, team, password))
             self.connection.commit()
+            bot.send_message(message.chat.id, 'Ви зареєстровані!')
         except sqlite3.IntegrityError:
             bot.send_message(message.chat.id, 'Ви вже зареєстровані!')
 
-        bot.send_message(message.chat.id, 'Ви зареєстровані!')
+    def create_table_viewer(self):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS viewer (id INTEGER PRIMARY KEY, nickname TEXT NOT NULL, "
+                            "password TEXT NOT NULL)")
+        self.connection.commit()
+
+    def add_user_viewer(self, message):
+        try:
+            self.cursor.execute("INSERT INTO viewer (id, nickname, password) VALUES (?, ?, ?)",
+                                (message.chat.id, nickname, password))
+            self.connection.commit()
+            bot.send_message(message.chat.id, 'Ви зареєстровані!')
+        except sqlite3.IntegrityError:
+            bot.send_message(message.chat.id, 'Ви вже зареєстровані!')
 
     def get_users(self):
         self.cursor.execute("SELECT * FROM users")
@@ -97,19 +111,37 @@ class Database:
         self.cursor.execute("SELECT * FROM participants")
         return self.cursor.fetchall()
 
+    def get_viewer(self):
+        self.cursor.execute("SELECT * FROM viewer")
+        return self.cursor.fetchall()
+
 
 db = Database('test1.db')
 
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
-    bot.reply_to(message, 'Привіт! Я бот реєстрації користувачів для змагань. Для реєстрації введи /register')
+    db.create_table()
+    if db.user_exists(message):
+        bot.send_message(message.chat.id, 'Ви вже зареєстровані!')
+    else:
+        bot.reply_to(message, 'Привіт! Я бот реєстрації користувачів для конкурсів. Для реєстрації введіть /register')
 
 
 @bot.message_handler(commands=['register'])
 def handle_register(message):
-    bot.reply_to(message, 'Введіть Ваше ім\'я')
-    bot.register_next_step_handler(message, process_name_step)
+    db.create_table()
+    if db.user_exists(message):
+        bot.send_message(message.chat.id, 'Ви вже зареєстровані!')
+    else:
+        db.add_user(message)
+        markup = telebot.types.InlineKeyboardMarkup()
+        user_button = telebot.types.InlineKeyboardButton('Глядач', callback_data='viewer')
+        organizer_button = telebot.types.InlineKeyboardButton('Організатор', callback_data='organizer')
+        jury_button = telebot.types.InlineKeyboardButton('Журі', callback_data='jury')
+        participant_button = telebot.types.InlineKeyboardButton('Учасник', callback_data='participant')
+        markup.add(user_button, organizer_button, jury_button, participant_button)
+        bot.send_message(message.chat.id, 'Обери свою роль', reply_markup=markup)
 
 
 def process_name_step(message):
@@ -126,52 +158,82 @@ def process_lastname_step(message):
     bot.register_next_step_handler(message, process_password_step)
 
 
+def process_company_step(message):
+    global company
+    company = message.text
+    add(message)
+
+
+def process_team_step(message):
+    global team
+    team = message.text
+    bot.reply_to(message, 'Введіть пароль')
+    bot.register_next_step_handler(message, process_password_step)
+
+
+def process_nickname_step(message):
+    global nickname
+    nickname = message.text
+    bot.reply_to(message, 'Введіть пароль')
+    bot.register_next_step_handler(message, process_password_step)
+
+
 def process_password_step(message):
     global password
+    global role
     password = message.text
-    # bot.reply_to(message, 'Обери роль')
-    markup = telebot.types.InlineKeyboardMarkup()
-    user_button = telebot.types.InlineKeyboardButton('Користувач', callback_data='user')
-    organizer_button = telebot.types.InlineKeyboardButton('Організатор', callback_data='organizer')
-    jury_button = telebot.types.InlineKeyboardButton('Журі', callback_data='jury')
-    participant_button = telebot.types.InlineKeyboardButton('Учасник', callback_data='participant')
-    markup.add(user_button, organizer_button, jury_button, participant_button)
-    bot.send_message(message.chat.id, 'Обери свою роль', reply_markup=markup)
+    if role == 'jury':
+        markup = telebot.types.InlineKeyboardMarkup()
+        yes_button = telebot.types.InlineKeyboardButton('Так', callback_data='yes')
+        no_button = telebot.types.InlineKeyboardButton('Ні', callback_data='no')
+        markup.add(yes_button, no_button)
+        bot.send_message(message.chat.id, 'Ви представляєте якусь компанію?', reply_markup=markup)
+    else:
+        add(message)
+
+
+def add(message):
+    global role
+    if role == 'viewer':
+        db.add_user_viewer(message)
+    elif role == 'organizer':
+        db.add_user_organizer(message)
+    elif role == 'jury':
+        db.add_user_jury(message)
+    elif role == 'participant':
+        db.add_user_participant(message)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback_query(call):
-    if call.data == 'user':
-        db.add_user(call.message)
+    global role
+    global check
+    if call.data == 'viewer':
+        role = 'viewer'
+        db.create_table_viewer()
+        bot.send_message(call.message.chat.id, 'Введіть Ваш нікнейм.')
+        bot.register_next_step_handler(call.message, process_nickname_step)
     elif call.data == 'organizer':
-        db.add_user_organizer(call.message)
+        role = 'organizer'
+        db.create_table_organizer()
+        bot.send_message(call.message.chat.id, 'Введіть Ваше ім\'я!')
+        bot.register_next_step_handler(call.message, process_name_step)
     elif call.data == 'jury':
-        db.add_user_jury(call.message, False)
+        role = 'jury'
+        db.create_table_jury()
+        bot.send_message(call.message.chat.id, 'Введіть Ваше ім\'я!')
+        bot.register_next_step_handler(call.message, process_name_step)
     elif call.data == 'participant':
-        db.add_user_participant(call.message)
-
-
-# @bot.callback_query_handler(func=lambda call: call.data in ['users', 'organizers', 'jury', 'participants'])
-# def handle_users_callback_query(call):
-#     role = call.data
-#     users = None
-#     if role == 'users':
-#         users = db.get_users()
-#     elif role == 'organizers':
-#         users = db.get_organizers()
-#     elif role == 'jury':
-#         users = db.get_jury()
-#     elif role == 'participants':
-#         users = db.get_participants()
-#
-#     info = 'Список користувачів::\n'
-#     if users:
-#         for u in users:
-#             info += f'Ім\'я: {u[1]}, Прізвище: {u[2]}\n'
-#     else:
-#         info = 'No users found.'
-#
-#     bot.send_message(call.message.chat.id, info)
+        role = 'participant'
+        db.create_table_participant()
+        bot.send_message(call.message.chat.id, 'Введіть команду, яку Ви представляєте.')
+        bot.register_next_step_handler(call.message, process_team_step)
+    elif call.data == 'yes':
+        check = True
+        bot.send_message(call.message.chat.id, 'Введіть компанію, яку Ви представляєте.')
+        bot.register_next_step_handler(call.message, process_company_step)
+    elif call.data == 'no':
+        add(call.message)
 
 
 bot.polling()
